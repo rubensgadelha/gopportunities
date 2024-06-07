9edf3c56963a18a4781e69818d60a3c77bdfc845
Rubens Gadelha <rubensgadelha@gmail.com>
2024-06-07 14:55:42 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/rubensgadelha/gopportunities/router"
+import (
+	"github.com/rubensgadelha/gopportunities/config"
+	"github.com/rubensgadelha/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Error("config initialization error:", err)
+		return
+	}
+
 	router.Initialize()
 }
